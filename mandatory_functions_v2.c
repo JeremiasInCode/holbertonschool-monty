619faccf4a465d0149b34afcd080f140bead3798
Jeremias Erba <jeremiaserba7894@gmail.com>
2023-11-18 13:16:48 -0300

    _nop function

--- mandatory_functions_v2.c
+++ mandatory_functions_v2.c
@@ -65,3 +65,10 @@ void _add(stack_t **stack, unsigned int line)
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
 }
+
+
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
