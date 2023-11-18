a35e34f3cbd3e5d032ef7c4ed874c7083d98b9cd
Jeremias Erba <jeremiaserba7894@gmail.com>
2023-11-18 13:15:44 -0300

    _swap method

--- mandatory_functions_v2.c
+++ mandatory_functions_v2.c
@@ -20,3 +20,25 @@ void _pop(stack_t **stack, unsigned int line_number)
 	*stack = (*stack)->next;
 	free(node);
 }
+
+/**
+* _swap - The opcode swap swaps the top two elements of the stack.
+*
+* @stack: stack where work will be done.
+* @line: number of lines.
+*/
+
+void _swap(stack_t **stack, unsigned int line)
+{
+	stack_t *check = (*stack)->next;
+
+	if (check->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line);
+		exit(EXIT_FAILURE);
+	}
+	int tmp_n = (*stack)->n;
+	stack_t *tmp = (*stack)->next;
+	(*stack)->n = tmp->n;
+	tmp->n = tmp_n;
+}
